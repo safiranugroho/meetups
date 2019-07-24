2b4d53eb7911f7461b1b35690f92a309662a3730
Safira Nugroho <snugroho@thoughtworks.com>
2019-07-24 23:03:06 +1000

    use rubocop bc i tried it and i like it

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -1,30 +1,48 @@
-require "./lib/helpers.rb"
+require './lib/helpers.rb'
 
 class TestHelpers
   include Helpers
 end
 
-describe "Helpers" do
+describe 'Helpers' do
   let(:helpers) { TestHelpers.new }
 
-  describe "#authorize" do
+  describe '#authorize' do
     let(:authorizer) { instance_double(Google::Auth::WebUserAuthorizer) }
     let(:request) { instance_double(Sinatra::Request) }
     let(:credentials) { instance_double(Google::Auth::UserRefreshCredentials) }
-    let(:fake_authorization_url) { "fake authorization url" }
+    let(:fake_authorization_url) { 'fake authorization url' }
 
-    before { allow(Google::Auth::WebUserAuthorizer).to receive(:new).and_return(authorizer) }
+    before do
+      allow(Google::Auth::WebUserAuthorizer).to receive(:new)
+        .and_return(authorizer)
+    end
 
-    context "when credentials are found" do
+    context 'when credentials are found' do
       subject { helpers.authorize request }
-      before { allow(authorizer).to receive(:get_credentials).with("me", request).and_return(credentials) }
+      before do
+        allow(authorizer).to receive(:get_credentials)
+          .with('me', request)
+          .and_return(credentials)
+      end
+
       it { is_expected.to eq credentials }
     end
 
-    context "when there are no credentials" do
+    context 'when there are no credentials' do
       subject { helpers.authorize request }
-      before { allow(authorizer).to receive(:get_credentials).with("me", request).and_return(nil) }
-      before { allow(authorizer).to receive(:get_authorization_url).with(login_hint: "me", request: request).and_return(fake_authorization_url) }
+
+      before do
+        allow(authorizer).to receive(:get_credentials)
+          .with('me', request)
+          .and_return(nil)
+      end
+
+      before do
+        allow(authorizer).to receive(:get_authorization_url)
+          .with(login_hint: 'me', request: request)
+          .and_return(fake_authorization_url)
+      end
 
       it { is_expected.to eq fake_authorization_url }
     end
