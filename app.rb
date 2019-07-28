26fd90bc9449e18c951ace4318e62bf88ef3b78a
Safira Nugroho <safiranugroho@gmail.com>
2019-07-28 16:51:43 +1000

    ditch tests for now and refactor app.rb

--- app.rb
+++ app.rb
@@ -8,57 +8,57 @@ require 'google/apis/gmail_v1'
 
 configure do
   Google::Apis::ClientOptions.default.application_name = ENV['APPLICATION_NAME']
+
   enable :sessions
   set :scope, Google::Apis::GmailV1::AUTH_GMAIL_COMPOSE
-
-  set :client_id, Google::Auth::ClientId.new(
-    ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
-  )
-
-  set :token_store, Google::Auth::Stores::FileTokenStore.new(
-    file: ENV['TOKEN_PATH']
-  )
+  set :client_id, Google::Auth::ClientId.new(ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'])
+  set :token_store, Google::Auth::Stores::FileTokenStore.new(file: ENV['TOKEN_PATH'])
 end
 
 helpers do
-  def authorize
+  def authorize(gmail_service)
     authorizer = Google::Auth::WebUserAuthorizer.new(
-      settings.client_id, settings.scope, settings.token_store, '/auth-callback'
+      settings.client_id,
+      settings.scope,
+      settings.token_store,
+      '/authorize-gmail-callback'
     )
 
     credentials = authorizer.get_credentials(ENV['DEFAULT_USER_ID'], request)
+    redirect authorizer.get_authorization_url(login_hint: ENV['DEFAULT_USER_ID'], request: request) if credentials.nil?
 
-    if credentials.nil?
-      redirect authorizer.get_authorization_url(
-        login_hint: ENV['DEFAULT_USER_ID'],
-        request: request
-      )
-    end
-    credentials
+    gmail_service.authorization = credentials
+  end
+
+  def compose_email
+    message = RMail::Message.new
+    message.header.to = ENV['EMAIL_RECIPIENT']
+    message.header.from = ENV['EMAIL_SENDER']
+    message.header.subject = ENV['EMAIL_SUBJECT']
+    message.body = 'Hiya!'
+
+    message
+  end
+
+  def send_email(gmail_service, message)
+    gmail_service.send_user_message(
+      ENV['DEFAULT_USER_ID'],
+      upload_source: StringIO.new(message.to_s),
+      content_type: 'message/rfc822'
+    )
+
+    puts "Message sent from #{ENV['EMAIL_SENDER']} to #{ENV['EMAIL_RECIPIENT']}!"
   end
 end
 
-get '/' do
-  service = Google::Apis::GmailV1::GmailService.new
-  service.client_options.application_name = ENV['APPLICATION_NAME']
-  service.authorization = authorize
+get '/authorize-gmail' do
+  gmail_service = Google::Apis::GmailV1::GmailService.new
 
-  message = RMail::Message.new
-  message.header.to = ENV['EMAIL_RECIPIENT']
-  message.header.from = ENV['EMAIL_SENDER']
-  message.header.subject = ENV['EMAIL_SUBJECT']
-  message.body = 'Hiya!'
-
-  service.send_user_message(
-    ENV['DEFAULT_USER_ID'],
-    upload_source: StringIO.new(message.to_s),
-    content_type: 'message/rfc822'
-  )
-
-  puts "Message sent from #{ENV['EMAIL_SENDER']} to #{ENV['EMAIL_RECIPIENT']}!"
+  authorize(gmail_service)
+  send_email(gmail_service, compose_email)
 end
 
-get '/auth-callback' do
+get '/authorize-gmail-callback' do
   redirect Google::Auth::WebUserAuthorizer
     .handle_auth_callback_deferred(request)
 end
