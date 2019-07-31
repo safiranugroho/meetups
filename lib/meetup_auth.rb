bef59207836c5d284636f2ef6138c0e5e4687efa
Safira Nugroho <safiranugroho@gmail.com>
2019-08-01 00:09:23 +1000

    fetch one event from one group and render it to html

--- lib/meetup_auth.rb
+++ lib/meetup_auth.rb
@@ -1,9 +1,14 @@
 require 'sinatra/base'
+require 'json'
+
 require './helpers/meetup_auth_helpers.rb'
 
 module WeeklyMeetups
   class MeetupAuth < Sinatra::Application
     enable :sessions
+
+    # find a better way to do this
+    set :views, settings.root + '/../views'
 
     helpers MeetupAuthHelpers
 
@@ -19,6 +24,9 @@ module WeeklyMeetups
 
       response = get_access_token params[:code]
       puts "Meetup.com access token- #{response}"
+
+      events = JSON.parse get_events_by_group 'ThoughtWorks-Melbourne'
+      slim :index, locals: { event: events.first }
     end
   end
 end
