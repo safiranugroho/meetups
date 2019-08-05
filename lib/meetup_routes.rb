05a57fb9035b0fab3c5d8ab4c47d80703b867f94
Safira Nugroho <safiranugroho@gmail.com>
2019-08-05 12:48:18 +1000

    fetch meetups only up to the next 7 days by default

--- lib/meetup_routes.rb
+++ lib/meetup_routes.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'json'
 require 'erb'
+require 'date'
 
 require './helpers/meetup_helpers.rb'
 
@@ -8,17 +9,20 @@ module WeeklyMeetups
   class MeetupRoutes < Sinatra::Application
     enable :sessions
 
+    set :root, File.dirname(__FILE__) + '/..'
+    set :views, settings.root + '/views'
+
     helpers MeetupHelpers
 
     get '/fetch-meetups' do
-      events = JSON.parse get_events_by_group 'ThoughtWorks-Melbourne'
+      events = JSON.parse get_events_by_group('Women-Who-Code-Melbourne')
       @event = events.first
 
       layout = File.read('./views/layout.erb')
       output = ERB.new(layout).result(binding)
 
       File.open('./views/output.html', 'w+') do |f|
-        f.write output
+        f.write(output)
       end
 
       erb :layout
