2b214d31b5a1ea648462cf2d0cf5a6cf126fec9a
Safira Nugroho <snugroho@thoughtworks.com>
2019-08-06 22:57:44 +1000

    remove function to get access token for Meetup.com because i missed it during the cleanup

--- helpers/meetup_helpers.rb
+++ helpers/meetup_helpers.rb
@@ -6,27 +6,6 @@ require 'json'
 
 module WeeklyMeetups
   module MeetupHelpers
-    def get_access_token(code)
-      header = { 'Content-Type': 'application/x-www-form-urlencoded' }
-      request_body = URI.encode_www_form(
-        client_id: ENV['MEETUP_CLIENT_ID'],
-        client_secret: ENV['MEETUP_CLIENT_SECRET'],
-        grant_type: 'authorization_code',
-        redirect_uri: "#{ENV['MEETUP_CALLBACK_HOST']}/authorize-meetup-callback",
-        code: code
-      )
-
-      uri = URI.parse('https://secure.meetup.com/oauth2/access')
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-
-      request = Net::HTTP::Post.new(uri.request_uri, header)
-      request.body = request_body
-
-      response = http.request(request)
-      response.body
-    end
-
     def get_events_by_group(group, number_of_extra_days = 7)
       meetup_earliest_date = Date.today.iso8601
       meetup_latest_date = (Date.today + number_of_extra_days).iso8601
