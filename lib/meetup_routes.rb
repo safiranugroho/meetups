bdb4005a15c7ebc782c0a8c23d04906db7f46216
Safira Nugroho <snugroho@thoughtworks.com>
2019-11-10 02:10:04 +1100

    remove unused code

--- lib/meetup_routes.rb
+++ lib/meetup_routes.rb
@@ -1,7 +1,6 @@
 require 'sinatra/base'
 require 'json'
 require 'erb'
-require 'date'
 
 require './helpers/meetup_helpers.rb'
 
@@ -14,89 +13,19 @@ module WeeklyMeetups
 
     helpers MeetupHelpers
 
-    meetup_groups = %w[
-      Female-Coders-Lab-Melbourne
-      Melbourne-Scala-User-Group
-      golang-mel
-      Infrastructure-Coders
-      Melbourne-Haskell-Users-Group
-      devops-melbourne
-      AWS-AUS
-      MelbourneMUG
-      Docker-Melbourne-Australia
-      AngularJS-Melbourne
-      Meteor-Melbourne
-      gdg-melbourne
-      Melbourne-Python-Meetup-Group
-      Swift-Devs-Melbourne
-      Melbourne-Apache-Spark-Meetup
-      React-Melbourne
-      scrum-12
-      Agile-Project-Managers-Melbourne
-      Agile-Business-Analysts-Melbourne
-      Application-Security-OWASP-Melbourne
-      Big-Data-Analytics-Meetup-Group
-      Ruby-On-Rails-Oceania-Melbourne
-      hack-for-privacy
-      BuzzConf
-      Melbourne-Java-JVM-Users-Group
-      AgileCoach
-      Kanban-Melbourne
-      Melbourne-Lean-Change-Management-Meetup
-      Cynefin-Melbourne-Meetup-Group
-      Responsive-Org-Melbourne
-      CTO-School-Melbourne
-      Product-Anonymous-Meetup-Melbourne
-      ProductTank-Melbourne
-      Melbourne-Lean-Coffee
-      Visual-Friends-Australasia
-      Design-Thinking-and-Business-Innovation-Melbourne
-      SecTalks-Melbourne
-      Melbourne-VR
-      The-UX-Design-Group-of-Melbourne
-      Melbourne-CocoaHeads
-      PyLadies-Melbourne
-      Melbourne-Functional-User-Group-MFUG
-      ThoughtWorks-Melbourne
-      Melbourne-Blender-Society
-      Machine-Learning-AI-Meetup
-      Data-Engineering-Melbourne
-      melbnlp
-      Melbourne-Women-in-Machine-Learning-and-Data-Science
-      Melbourne-DevSecOps-User-Group
-      Melbourne-Kubernetes-Meetup
-      GDG-Cloud-Melbourne
-      Melbourne-Creative-AI-Meetup
-      melbourne-search
-      codelikeagirlau
-      Women-Who-Code-Melbourne
-      Disruptors-In-Tech-Melb
-      DDD-Melbourne-By-Night
-      Elm-Melbourne
-      Melbourne-ML-AI-Bookclub
-      Melbourne-Kotlin-Meetup
-      Junior-Developers-Melbourne
-      Melbourne-Docker-User-Group
-      the-web
-      Melbourne-NET-User-Group
-      Cyberspectrum-Melbourne
-      Enterprise-Data-Science-Architecture
-      DeepRacer-Nights
-      Melbourne-APIs-Meetup
-      slack-platform-community-melbourne
-      GraphQL-Melbourne
-      TECHLED-Melbourne
-      Melbourne-PE
-    ]
+    get '/authorize-meetup' do
+      redirect 'https://secure.meetup.com/oauth2/authorize'\
+              '?client_id=nin8u7bna69vbrictum1rcve4l'\
+              '&response_type=code'\
+              '&redirect_uri=http://localhost:4567/fetch-meetups'
+    end
 
     get '/fetch-meetups' do
-      all_events = []
-      meetup_groups.each do |group|
-        events_by_group = JSON.parse get_events_by_group(group)
-        all_events = all_events.concat(events_by_group) unless events_by_group.instance_of? Hash
-      end
+      response = get_access_token(params[:code])
+      json = JSON.parse response.body
 
-      @events_by_date = sort_events_by_date(all_events) unless all_events.empty?
+      events = JSON.parse get_upcoming_events(json['access_token'])
+      @events_by_date = sort_events_by_date(events['data']['events']) unless events.empty?
 
       email_content = File.read('./views/email_content.erb')
       output = ERB.new(email_content).result(binding)
