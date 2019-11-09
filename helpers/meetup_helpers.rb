7b890b83fd0e3aa915a87e12352436c963588c1b
Safira Nugroho <snugroho@thoughtworks.com>
2019-11-10 00:42:29 +1100

    authorise meetup and send gql query with the access token

--- helpers/meetup_helpers.rb
+++ helpers/meetup_helpers.rb
@@ -23,6 +23,43 @@ module WeeklyMeetups
       response.body
     end
 
+    def get_access_token(code)
+      header = { 'Content-Type': 'application/x-www-form-urlencoded' }
+      request_body = URI.encode_www_form(
+        client_id: ENV['MEETUP_CLIENT_ID'],
+        client_secret: ENV['MEETUP_CLIENT_SECRET'],
+        grant_type: 'authorization_code',
+        redirect_uri: 'http://localhost:4567/authorize-meetup-callback',
+        code: code
+      )
+
+      uri = URI.parse('https://secure.meetup.com/oauth2/access')
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+
+      request = Net::HTTP::Post.new(uri.request_uri, header)
+      request.body = request_body
+
+      response = http.request(request)
+      response
+    end
+
+    def get_upcoming_events(access_token, number_of_extra_days = 7)
+      uri = URI.parse("http://localhost:4000/graphql")
+      http = Net::HTTP.new(uri.host, uri.port)
+
+      header = { 'Content-Type': 'application/json', 'Authorization': "Bearer #{access_token}"}
+      body = {
+        "query": "{ events(input: { category: \"292\", daysInAdvance: 7  }) { name day date time venue link } }"
+      }
+
+      request = Net::HTTP::Post.new(uri.request_uri, header)
+      request.body = body.to_json
+
+      response = http.request(request)
+      response.body
+    end
+
     def sort_events_by_date(events)
       events_by_date = Hash.new { |hash, key| hash[key] = [] }
 
